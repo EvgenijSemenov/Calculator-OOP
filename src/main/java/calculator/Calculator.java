abb58ec215f6941b244862bf10c0204b81f5af7c
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-24 12:40:55 +0300

    Show calculator frame on application start up

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package main.java.calculator;
 
+import main.java.calculator.view.CalculatorFrame;
+
 public class Calculator {
+
+    public static void main(String[] args) {
+        CalculatorFrame calculatorFrame = new CalculatorFrame();
+    }
 
 }
