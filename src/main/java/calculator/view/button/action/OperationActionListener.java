b9a8e755eb4a19d772c66edd08d3d418e94de1f6
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-28 07:57:22 +0300

    Create OperationActionListener class

---
+++ src/main/java/calculator/view/button/action/OperationActionListener.java
@@ -0,0 +1,34 @@
+package main.java.calculator.view.button.action;
+
+import main.java.calculator.model.Calculator;
+import main.java.calculator.view.DisplayPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OperationActionListener implements ActionListener {
+
+    private DisplayPanel displayPanel;
+
+    public OperationActionListener(DisplayPanel displayPanel) {
+        this.displayPanel = displayPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String expression = displayPanel.getExpression();
+        expression += " " + displayPanel.getResult() + " " + e.getActionCommand();
+        expression = expression.trim();
+
+        //TODO fix using setExpression method twice
+        displayPanel.setExpression(expression);
+
+        if (expression.length() > 3) {
+            String expressionResult = new Calculator().evaluate(expression.substring(0, expression.length() - 2)).toString();
+            displayPanel.updateResult(expressionResult);
+        }
+
+        displayPanel.setExpression(expression);
+    }
+
+}
