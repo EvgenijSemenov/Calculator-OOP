b63586abdad3693e301bbac8eca073ce3ec4f9e1
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-28 07:51:29 +0300

    Create double value class

---
+++ src/main/java/calculator/model/value/DoubleValue.java
@@ -0,0 +1,73 @@
+package main.java.calculator.model.value;
+
+public class DoubleValue implements Value{
+
+    protected Double value;
+
+    public DoubleValue(Double doubleValue) {
+        value = doubleValue;
+    }
+
+    public DoubleValue(String doubleValue) {
+        value = Double.parseDouble(doubleValue);
+    }
+
+    @Override
+    public String toString() {
+        String result = Double.toString(value);
+        if (value % 1 == 0) {
+            return Integer.toString(value.intValue());
+        }
+        return result;
+    }
+
+    @Override
+    public Value add(Value value) {
+        Double result = this.value + Double.parseDouble(value.toString());
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value multiply(Value value) {
+        Double result = this.value * Double.parseDouble(value.toString());
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value substract(Value value) {
+        Double result = this.value - Double.parseDouble(value.toString());
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value divide(Value value) {
+        Double result = this.value / Double.parseDouble(value.toString());
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value percentage(Value value) {
+        Double result = this.value / 100 * Double.parseDouble(value.toString());
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value root(Value value) {
+        Double result = Math.pow(this.value, 1.0 / Double.parseDouble(value.toString()));
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+    @Override
+    public Value power(Value value) {
+        Double result = Math.pow(this.value, Double.parseDouble(value.toString()));
+
+        return new DoubleValue(Double.toString(result));
+    }
+
+}
