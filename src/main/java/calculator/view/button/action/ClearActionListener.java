de70758bc7937d408ec8d3b850b4522d15524739
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-28 07:56:26 +0300

    Create ClearActionListener class

---
+++ src/main/java/calculator/view/button/action/ClearActionListener.java
@@ -0,0 +1,18 @@
+package main.java.calculator.view.button.action;
+
+import main.java.calculator.view.DisplayPanel;
+
+import java.awt.event.ActionEvent;
+
+public class ClearActionListener extends CalculatorActionListener {
+
+    public ClearActionListener(DisplayPanel displayPanel) {
+        super(displayPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        displayPanel.clearAll();
+    }
+
+}
