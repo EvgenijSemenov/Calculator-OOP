63a229a297f680d35059aca9101e68aa85e81960
YevheniiSemenov <semenovevgenij92@gmail.com>
2017-06-28 07:56:39 +0300

    Create ClearResultActionListener class

---
+++ src/main/java/calculator/view/button/action/ClearResultActionListener.java
@@ -0,0 +1,17 @@
+package main.java.calculator.view.button.action;
+
+import main.java.calculator.view.DisplayPanel;
+
+import java.awt.event.ActionEvent;
+
+public class ClearResultActionListener extends CalculatorActionListener {
+
+    public ClearResultActionListener(DisplayPanel displayPanel) {
+        super(displayPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        displayPanel.clearResult();
+    }
+}
